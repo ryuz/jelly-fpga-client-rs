d54a04d1096907eef197e0711263891e2f653b61
Ryuji Fuchikami <ryuji.fuchikami@nifty.com>
2025-10-19 15:01:38 +0900

    Update dependencies and add jelly-fpga-server submodule

--- build.rs
+++ build.rs
@@ -1,4 +1,4 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("proto/jelly_fpga_control.proto")?;
+    tonic_prost_build::compile_protos("jelly-fpga-server/protos/jelly_fpga_control.proto")?;
     Ok(())
 }
